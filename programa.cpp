5b07beccbcbb85cba494dafb2665936c4d6d9cdc
jamesmoose <jameslawlietmoose@gmail.com>
2017-12-12 19:06:53 -0600

    Funcion suma

--- programa.cpp
+++ programa.cpp
@@ -1,8 +1,18 @@
 #include<iostream>
 using namespace std;
+void sum();
 int main(){
 	system("color b1");
-	cout<<"hola mundo #recuperacion #salvemedelfiltro :v";
+	cout<<"hola mundo #recuperacion #salvemedelfiltro :v\n\n\n";
+	sum();
 	cin.get();
 	return 0;
+}
+void sum(){
+	int a,b;
+	cout<<"inserta primer numero:\n";
+	cin>>a;
+	cout<<"inserta segundo numero:\n";
+	cin>>b;
+	cout<<"la suma de los dos numeros es: "<<a+b;
 }
