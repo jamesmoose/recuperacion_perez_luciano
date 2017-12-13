c54da3b1468b1fdc86ed9dd610b8ef5a6cfe3ea0
jamesmoose <jameslawlietmoose@gmail.com>
2017-12-12 19:17:59 -0600

    Factorial

--- programa.cpp
+++ programa.cpp
@@ -1,10 +1,16 @@
 #include<iostream>
 using namespace std;
 void sum();
+long double factorial(int n);
 int main(){
+	int n;
 	system("color b1");
 	cout<<"hola mundo #recuperacion #salvemedelfiltro :v\n\n\n";
 	sum();
+	cout<<"\n\ninserte numero: \n";
+	cin>>n;
+	cout<<"\nel factorial de "<<n<<" es "<<factorial(n);
+	
 	cin.get();
 	return 0;
 }
@@ -15,4 +21,11 @@ void sum(){
 	cout<<"inserta segundo numero:\n";
 	cin>>b;
 	cout<<"la suma de los dos numeros es: "<<a+b;
+}
+long double factorial(int n){
+    long double fact;
+    if (n==0)
+        return 1;
+    else
+         return n*factorial(n-1);
 }
